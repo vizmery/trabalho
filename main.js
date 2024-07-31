31a50688f306db11e1270c1a9d9df84460f17bd0
vizmery <vizmery.tonini@escola.pr.gov.br>
2024-07-31 08:47:39 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -41,3 +41,40 @@ const perguntas = [
         ]
     },
 ]
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){ 
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativa){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas)
+    }
+}
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", function(){
+            atual++;
+            mostraPergunta();
+        })
+        caixaAlternativas.appendChild(botaoAlternativas)
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+atual++;
+mostraPergunta();
+}
+
+
+mostraPergunta();
